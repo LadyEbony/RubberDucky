75e6f111e2734e33982d51d4ff19a16a2017ca9d
ychen133 <ychen133@ucr.edu>
2017-01-21 01:09:06 -0800

    Made KillUrchins, ExitDrain

---
+++ Rubberducky Simulator/Assets/Scripts/KillUrchin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillUrchin : MonoBehaviour {
+
+	private PolygonCollider2D urchinCollider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if(coll.gameObject.tag == "Player")
+		{
+			coll.gameObject.SetActive(false);	//"kill" Player object
+			//Game Over
+		}
+	}
+}
