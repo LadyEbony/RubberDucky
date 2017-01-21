1ac0f014f5f7e8910f16194aa796e632cb2d7f34
Jose Garcia <josegarcia8064@gmail.com>
2017-01-20 21:12:25 -0800

    v0.01
    This should work

---
+++ Rubberducky Simulator/Assets/Scripts/SpawnWave.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWave : MonoBehaviour {
+
+    public GameObject RippleObject;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        float mousex = Input.mousePosition.x;
+        float mousey = Input.mousePosition.y;
+        Debug.Log("x: " + mousex + "| y: " + mousey);
+    }
+}
