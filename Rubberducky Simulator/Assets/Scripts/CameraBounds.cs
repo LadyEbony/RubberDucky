0e8f6bb24f8d7a188564ac1cf8910be1354997bd
ddiep003@ucr.edu <ddiep003@ucr.edu>
2017-01-21 19:21:40 -0800

    Player cannot leave screen x = 11.1 y = 5

---
+++ Rubberducky Simulator/Assets/Scripts/CameraBounds.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBounds : MonoBehaviour {
+
+    // Use this for initialization
+    public float camerax;
+    public float cameray;
+	void Start () {
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // X axis
+        if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector2(-camerax, transform.position.y);
+        }
+        else if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector2(camerax, transform.position.y);
+        }
+
+        // Y axis
+        if (transform.position.y <= -5f)
+        {
+            transform.position = new Vector2(transform.position.x, -cameray);
+        }
+        else if (transform.position.y >= 5f)
+        {
+            transform.position = new Vector2(transform.position.x, cameray);
+        }
+    }
+}
